c3e5d1c79ca5399476c34340b53a541ee80dd1e6
Rikas <rrikas341@gmail.com>
2026-07-15 15:06:34 +0530

    Update lecture3.py

--- lecture3.py
+++ lecture3.py
@@ -36,3 +36,73 @@ t=("John",24,"Jaffna")
 24
 >>>
 
+>>> hash("john")
+-3134102245315481563
+>>>
+>>> hash("John")
+-6675318360349301779
+>>>
+>>> hash(10)
+10
+>>>
+>>> hash(12.356)
+820880111280074764
+>>>
+>>> hash(123456)
+123456
+>>>
+
+>>> hash((1,2,3))   #Bcz, Tuple is immutable(Can't change).list is mutable,int , string are immutable
+529344067295497451
+
+>>> hash([1,2,3,4])
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unhashable type: 'list'
+>>>
+
+>>> {('A','B'):'Name'}  #Immutable and immutable
+{('A', 'B'): 'Name'}
+>>>
+
+
+>>> s1={"ATG","ACG","AGG"}
+>>> s2={"ATG","ACC","AGG"}
+>>> s1+s2
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unsupported operand type(s) for +: 'set' and 'set'
+>>>
+>>> s1 - s2
+{'ACG'}
+>>> s2 - s1
+{'ACC'}
+
+>>> s1.union(s2)
+{'AGG', 'ACG', 'ATG', 'ACC'}
+>>>
+>>> s1.intersection(s2)
+{'AGG', 'ATG'}
+>>>
+>>> s1 & s2
+{'AGG', 'ATG'}
+>>>
+>>> s1 | s2
+{'AGG', 'ACG', 'ATG', 'ACC'}
+>>>
+>>> list(range(0,10,2))
+[0, 2, 4, 6, 8]
+>>>
+>>> evens=[x for x in range(0,10,2) if x%2==0]
+>>>
+>>> evens
+[0, 2, 4, 6, 8]
+>>>
+>>> r=range(0,10,2)
+>>> r[0]
+0
+>>>
+>>> r[1]
+2
+>>>
+
