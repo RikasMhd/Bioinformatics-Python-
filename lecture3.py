e4f559427b002f2aaf824b998abb2652495e666e
Rikas <rrikas341@gmail.com>
2026-07-15 13:40:23 +0530

    Update lecture3.py

--- lecture3.py
+++ lecture3.py
@@ -11,3 +11,28 @@
 >>> ls1=[8,8,1,5,0,2,4,2,1,7]
 >>> ls1.count(2)
 2
+
+>>> ls1=[8,8,1,5,0,2,4,2,1,7]
+>>> ls1.count(2)
+2
+>>>
+>>> ls1.index(5)
+3
+>>>
+>>> ls1.index(8)
+0
+>>>
+
+t=("John",24,"Jaffna")
+>>> name,age,city=t[0],t[1],t[2]
+>>> name,age,city
+('John', 24, 'Jaffna')
+>>>
+>>> name
+'John'
+>>> city
+'Jaffna'
+>>> age
+24
+>>>
+
